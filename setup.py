5047fa4eed47c1e58b7fac1ef853d8da737b1e74
dmparker0 <dmccracken123@gmail.com>
2018-10-22 13:45:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Dan Parker',
     author_email='dan.m.parker0@gmail.com',
     packages=['nflsim'],
-    install_requires=['numpy,'pandas','scipy','bs4','requests','joblib'],
+    install_requires=['numpy','pandas','scipy','bs4','requests','joblib'],
     version='0.1',
     license='MIT',
     description='A tool for simulating the NFL regular season and playoffs',
