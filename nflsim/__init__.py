b25a8f087c389953da9b4c11c422007993fc4512
dmparker0 <dmccracken123@gmail.com>
2018-10-23 15:11:31 -0600

    Update __init__.py

--- nflsim/__init__.py
+++ nflsim/__init__.py
@@ -1,3 +1,3 @@
-from .core import Sim, Simulation, Simulations
+from .core import Simulate, Simulation, Simulations
 from .regression import Regression
 from .pwr import PWRsystems, PWR, SRS, FPI, DVOA, Sagarin
