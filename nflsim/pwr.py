b25646a2f367656182438530f447806595ff02d3
dmparker0 <dmccracken123@gmail.com>
2018-10-23 10:50:41 -0600

    Update pwr.py

--- nflsim/pwr.py
+++ nflsim/pwr.py
@@ -147,7 +147,7 @@ class PWRsystems(object):
     def __init__(self, regress_to=None, custom=False, srs=None, fpi=None, dvoa=None, sagarin=None, others=None):
         self.regress_to = regress_to
         self.systems = []
-        if not custom:
+        if (srs is None) and (fpi is None) and (dvoa is None) and (sagarin is None) and (others is None):
             self.systems.append(SRS())
             self.systems.append(FPI())
             self.systems.append(DVOA())
