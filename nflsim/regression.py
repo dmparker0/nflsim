0256ffeb513cbc961d35724f7c189843e9cf21f3
dmparker0 <dmccracken123@gmail.com>
2018-10-23 11:50:13 -0600

    Update regression.py

--- nflsim/regression.py
+++ nflsim/regression.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Regression(object):
     def __init__(self, to=0, weight=None, n_games=16, basecol='Baseline'):
@@ -20,9 +21,9 @@ class Regression(object):
             played_weight = (1 - self.regression_weight) * df.shape[0]
             weight_sum = 1
         else:
-            reg_weight = self.num_games - mydf['Games Played'].values
+            reg_weight = np.maximum(self.num_games - mydf['Games Played'].values, [0] * mydf.shape[0])
             played_weight = mydf['Games Played'].values
-            weight_sum = self.num_games
+            weight_sum = reg_weight + played_weight
         played_weighted = mydf[pwrcol].values * played_weight
         regressed_weighted =  reg_values * reg_weight
         return (played_weighted + regressed_weighted) / weight_sum
