e89490ed0da7ddfbd3522a6e46c272abd4cceff9
Dan Parker <dmccracken123@gmail.com>
2019-11-08 13:15:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='A tool for simulating the NFL regular season and playoffs',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    download_url = 'https://github.com/dmparker0/nflsim/archive/v1.1.3.tar.gz',
+    download_url = 'https://github.com/dmparker0/nflsim/archive/v1.1.4.tar.gz',
     keywords = ['NFL', 'football', 'sports','simulation','statistics'], 
 )
 
