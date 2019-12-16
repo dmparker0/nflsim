524a8a64ab47f61ec742f07acad1bc4f3240e6da
Dan Parker <dmccracken123@gmail.com>
2019-12-16 13:25:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     author_email='dan.m.parker0@gmail.com',
     packages=['nflsim'],
     install_requires=['numpy','pandas','scipy','bs4','requests','joblib'],
-    version='1.1.6',
+    version='1.1.7',
     license='MIT',
     description='A tool for simulating the NFL regular season and playoffs',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    download_url = 'https://github.com/dmparker0/nflsim/archive/v1.1.6.tar.gz',
+    download_url = 'https://github.com/dmparker0/nflsim/archive/v1.1.7.tar.gz',
     keywords = ['NFL', 'football', 'sports','simulation','statistics'], 
 )
 
